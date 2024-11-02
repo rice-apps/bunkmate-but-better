a81bec3e8294b38c30584a19b08b15546110fbdf
rtshah <rahultshah@gmail.com>
2024-11-02 14:51:00 -0500

    building navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,9 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
- 
+import { Calendar } from "@/components/ui/calendar";
+import { Popover, PopoverContent, PopoverTrigger } from "@radix-ui/react-popover";
+
 
 import { useState } from 'react';
 
@@ -25,15 +27,53 @@ import Link from 'next/link';
 // <FaMagnifyingGlass />
 // 
 
-const Navbar = () => {
+interface ModularDropDownProps {
+  allOptions: string[];
+  title: string;
+}
 
-  const [selectedOption, setSelectedOption] = useState("Search Properties");
-  const [startDate, setStartDate] = useState("Select Date");
-  const [endDate, setEndDate] = useState("Select Date");
+const ModularDropDown: React.FC<ModularDropDownProps> = ({ allOptions, title }) => {
+  const [selectedOption, setSelectedOption] = useState(title);
 
+  const MenuItem: React.FC<{ option: string }> = ({ option }) => {
+    return (
+      <>
+        <DropdownMenuItem key={option} onClick={() => setSelectedOption(option)} className="flex justify-center">
+          <p className='hover:text-[#FF7439] text-center'>{option}</p>
+        </DropdownMenuItem>
+      </>
+    )
+  }
 
   return (
-    <div className='my-10 mx-10 flex flex-row place-items-center gap-[100px]'>
+    <>
+      <DropdownMenu key={title}>
+        <DropdownMenuTrigger asChild>
+          <button className='text-left max-w-full'>
+            <p className={`text-[14px] font-light ${selectedOption !== title ? "text-[#FF7439] font-bold" : "text-[#777777]"}`}>
+              {selectedOption}
+            </p>
+          </button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent className=''>
+          {allOptions.map((option) => {
+            return (
+              <MenuItem option={option} key={option} />
+            );
+          })}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
+
+const Navbar = () => {
+  const [startDate, setStartDate] = useState<Date | undefined>(new Date());
+  const [endDate, setEndDate] = useState<Date | undefined>(new Date());
+
+  return (
+    <div className='my-10 mx-10 flex flex-row place-items-center xl:gap-[5%] lg:gap-[5%] md:gap-[3%] sm:gap-[1%]'>
       <button className='hover:scale-110 transition-transform duration:100'>
         <Link href='/' className='flex flex-row gap-[8.33] place-items-center'>
           <Image src="/bunkmate_logo.png" alt="Bunkmate Logo" width={35.48} height={35.48} className='h-[35.48px] w-[35.48px]' />
@@ -41,45 +81,45 @@ const Navbar = () => {
         </Link>
       </button>
 
-      <div className="w-[700px] h-[78px] border-[2px] border-[#D9D9D9] rounded-[50px] shadow-lg flex place-items-center justify-center">
-        <div className='w-[657px] h-[41px] justify-center place-items-center flex flex-row whitespace-nowrap gap-5'>
-          <div className='grid grid-rows-2 gap-[2px] border-r pr-10'>
+      <div className="w-[50vw] h-[12.5vh] border-[2px] border-[#D9D9D9] rounded-[50px] shadow-lg flex place-items-center justify-center">
+        <div className='max-w-full h-[41px] justify-center place-items-center flex flex-row whitespace-nowrap'>
+          <div className='grid grid-rows-2 gap-[2px] border-r sm:px-1 md:px-3 lg:px-6 xl:px-8'>
             <p className='text-[14px] font-semibold text-[#777777]'>Distance from Rice</p>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <button className='text-left'>
-                  <p className={`text-[14px] font-light ${selectedOption !== "Search Properties" ? "text-[#FF7439] font-bold" : "text-[#777777]"}`}>
-                    {selectedOption}
-                  </p>
-                </button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className=''>
-                <DropdownMenuItem onClick={() => setSelectedOption("< 1 mile")} className="flex justify-center">
-                  <p className='hover:text-[#FF7439] text-center'>&lt; 1 mile</p>
-                  </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setSelectedOption("< 3 miles")} className="flex justify-center">
-                  <p className='hover:text-[#FF7439] text-center'>&lt; 3 miles</p>
-                  </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setSelectedOption("< 5 miles")} className="flex justify-center">
-                  <p className='hover:text-[#FF7439] text-center'>&lt; 5 miles</p>
-                  </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setSelectedOption("> 5 miles")} className="flex justify-center">
-                  <p className='hover:text-[#FF7439] text-center'>&gt; 5 miles</p>
-                  </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <ModularDropDown allOptions={["< 1 mile", "< 3 miles", "< 5 miles", "> 5 miles"]} title={"Search Properties"} />
           </div>
-          <div className='grid grid-rows-2 gap-[2px] border-r px-10 text-left'>
+          <div className='grid grid-rows-2 gap-[2px] border-r sm:px-1 md:px-3 lg:px-6 xl:px-8 text-left'>
             <p className='text-[14px] font-semibold text-[#777777]'>Start Date</p>
-            <button className='text-left'>
-              <p className='text-[14px] font-light text-[#777777]'>{ startDate }</p>
-            </button>
+            <Popover>
+              <PopoverTrigger asChild>
+                <button className='text-left'>
+                  <p className={`text-[14px] font-light ${startDate && startDate.toDateString() !== "Select Date" ? "text-[#FF7439] font-bold" : "text-[#777777]"}`}>{startDate ? startDate.toDateString() : "Select Date"}</p>
+                </button>
+              </PopoverTrigger>
+              <PopoverContent className="p-2 bg-white rounded-lg shadow-lg" style={{ zIndex: 1000 }}>
+                <Calendar
+                        mode="single"
+                        selected={startDate}
+                        onSelect={setStartDate}
+                />
+              </PopoverContent>
+            </Popover>
           </div>
-          <div className='grid grid-rows-2 gap-[2px] px-10'>
+          <div className='grid grid-rows-2 gap-[2px] sm:px-1 md:px-3 lg:px-6 xl:px-8'>
             <p className='text-[14px] font-semibold text-[#777777]'>End Date</p>
-            <button className='text-left'>
-              <p className='text-[14px] font-light text-[#777777]'>{ endDate }</p>
-            </button>
+            <Popover>
+              <PopoverTrigger asChild>
+                <button className='text-left'>
+                <p className={`text-[14px] font-light ${endDate && endDate.toDateString() !== "Select Date" ? "text-[#FF7439] font-bold" : "text-[#777777]"}`}>{endDate ? endDate.toDateString() : "Select Date"}</p>
+                </button>
+              </PopoverTrigger>
+              <PopoverContent className="p-2 bg-white rounded-lg shadow-lg" style={{ zIndex: 1000 }}>
+              <Calendar
+                        mode="single"
+                        selected={endDate}
+                        onSelect={setEndDate}
+                />
+              </PopoverContent>
+            </Popover>
           </div>
           <button>
             <FaMagnifyingGlass
