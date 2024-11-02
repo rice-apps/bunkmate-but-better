be30c31e9bd1ea603d0da38dca24c0b28518666a
ericg4 <eric.g4@outlook.com>
2024-11-02 16:00:07 -0500

    added some responsiveness and calendar date selection

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -49,8 +49,8 @@ const ModularDropDown: React.FC<ModularDropDownProps> = ({ allOptions, title }) 
     <>
       <DropdownMenu key={title}>
         <DropdownMenuTrigger asChild>
-          <button className='text-left max-w-full'>
-            <p className={`text-[14px] font-light ${selectedOption !== title ? "text-[#FF7439] font-bold" : "text-[#777777]"}`}>
+          <button className='text-left'>
+            <p className={`text-[14px] font-light ${selectedOption !== title ? "text-[#FF7439] font-extrabold" : "text-[#777777]"}`}>
               {selectedOption}
             </p>
           </button>
@@ -68,65 +68,55 @@ const ModularDropDown: React.FC<ModularDropDownProps> = ({ allOptions, title }) 
   );
 }
 
-const Navbar = () => {
-  const [startDate, setStartDate] = useState<Date | undefined>(new Date());
-  const [endDate, setEndDate] = useState<Date | undefined>(new Date());
+const ModularCalendarSelect: React.FC<{ title: string, defaultVal: string, border: boolean}> = ({title, defaultVal, border}) => {
+  const [date, setDate] = useState<Date | undefined>(undefined);
 
   return (
-    <div className='my-10 mx-10 flex flex-row place-items-center xl:gap-[5%] lg:gap-[5%] md:gap-[3%] sm:gap-[1%]'>
-      <button className='hover:scale-110 transition-transform duration:100'>
+    <div className={`grid grid-rows-2 gap-[2px] w-[167px] sm:px-1 md:px-3 lg:px-6 xl:px-8 text-left ${border ? "border-r" : ""}`}>
+      <p className='text-[14px] font-semibold text-[#777777]'>{title}</p>
+      <Popover>
+        <PopoverTrigger asChild>
+          <button className='text-left'>
+            <p className={`text-[14px] font-light ${date && date.toDateString() !== defaultVal ? "text-[#FF7439] font-extrabold" : "text-[#777777]"}`}>{date ? date.toDateString() : defaultVal}</p>
+          </button>
+        </PopoverTrigger>
+        <PopoverContent className="p-2 bg-white rounded-lg shadow-lg" style={{ zIndex: 1000 }}>
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+};
+
+const Navbar = () => {
+  const [startDate, setStartDate] = useState<Date | undefined>(undefined);
+  const [endDate, setEndDate] = useState<Date | undefined>(undefined);
+
+  return (
+    <div className='my-10 mx-10 flex flex-row place-items-center xl:gap-[5%] lg:gap-[4%] md:gap-[3%] sm:gap-[1%]'>
+      <button className='hover:scale-110 transition-transform duration:100 w-full'>
         <Link href='/' className='flex flex-row gap-[8.33] place-items-center'>
           <Image src="/bunkmate_logo.png" alt="Bunkmate Logo" width={35.48} height={35.48} className='h-[35.48px] w-[35.48px]' />
           <p className="ml-4 text-[30px] text-[#FF7439] font-semibold">bunkmate</p>
         </Link>
       </button>
 
-      <div className="w-[50vw] h-[12.5vh] border-[2px] border-[#D9D9D9] rounded-[50px] shadow-lg flex place-items-center justify-center">
-        <div className='max-w-full h-[41px] justify-center place-items-center flex flex-row whitespace-nowrap'>
-          <div className='grid grid-rows-2 gap-[2px] border-r sm:px-1 md:px-3 lg:px-6 xl:px-8'>
-            <p className='text-[14px] font-semibold text-[#777777]'>Distance from Rice</p>
-            <ModularDropDown allOptions={["< 1 mile", "< 3 miles", "< 5 miles", "> 5 miles"]} title={"Search Properties"} />
-          </div>
-          <div className='grid grid-rows-2 gap-[2px] border-r sm:px-1 md:px-3 lg:px-6 xl:px-8 text-left'>
-            <p className='text-[14px] font-semibold text-[#777777]'>Start Date</p>
-            <Popover>
-              <PopoverTrigger asChild>
-                <button className='text-left'>
-                  <p className={`text-[14px] font-light ${startDate && startDate.toDateString() !== "Select Date" ? "text-[#FF7439] font-bold" : "text-[#777777]"}`}>{startDate ? startDate.toDateString() : "Select Date"}</p>
-                </button>
-              </PopoverTrigger>
-              <PopoverContent className="p-2 bg-white rounded-lg shadow-lg" style={{ zIndex: 1000 }}>
-                <Calendar
-                        mode="single"
-                        selected={startDate}
-                        onSelect={setStartDate}
-                />
-              </PopoverContent>
-            </Popover>
-          </div>
-          <div className='grid grid-rows-2 gap-[2px] sm:px-1 md:px-3 lg:px-6 xl:px-8'>
-            <p className='text-[14px] font-semibold text-[#777777]'>End Date</p>
-            <Popover>
-              <PopoverTrigger asChild>
-                <button className='text-left'>
-                <p className={`text-[14px] font-light ${endDate && endDate.toDateString() !== "Select Date" ? "text-[#FF7439] font-bold" : "text-[#777777]"}`}>{endDate ? endDate.toDateString() : "Select Date"}</p>
-                </button>
-              </PopoverTrigger>
-              <PopoverContent className="p-2 bg-white rounded-lg shadow-lg" style={{ zIndex: 1000 }}>
-              <Calendar
-                        mode="single"
-                        selected={endDate}
-                        onSelect={setEndDate}
-                />
-              </PopoverContent>
-            </Popover>
-          </div>
-          <button>
-            <FaMagnifyingGlass
-              className='hover:cursor-pointer h-[29px] w-[25px] transition-transform duration-100 text-[#FF7439] hover:text-[#BB5529] hover:scale-105'
-            />
-          </button>
+      <div className="w-[50vw] h-[12.5vh] border-[2px] border-[#D9D9D9] rounded-[50px] shadow-lg flex flex-row place-items-center justify-center whitespace-nowrap">
+        <div className='grid grid-rows-2 gap-[2px] border-r pl-8 sm:pr-1 md:pr-3 lg:pr-4 xl:pr-8'>
+          <p className='text-[14px] font-semibold text-[#777777]'>Distance from Rice</p>
+          <ModularDropDown allOptions={["< 1 mile", "< 3 miles", "< 5 miles", "> 5 miles"]} title={"Search Properties"} />
         </div>
+        <ModularCalendarSelect title={"Start Date"} defaultVal={"Select Start Date"} border={true}/>
+        <ModularCalendarSelect title={"Start Date"} defaultVal={"Select End Date"} border={false}/>
+        <button className='pr-8'>
+          <FaMagnifyingGlass
+            className='hover:cursor-pointer h-[29px] w-[25px] transition-transform duration-100 text-[#FF7439] hover:text-[#BB5529] hover:scale-105'
+          />
+        </button>
       </div>
 
       <div className='flex flex-row gap-[20px]'>
