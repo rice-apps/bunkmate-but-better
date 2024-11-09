ad2b78f1f12fd5c38573fd4cc391b7d11cc80b5a
Leonard Gabriel Ong <45550118+leonriel@users.noreply.github.com>
2024-11-09 13:19:04 -0600

    Removed unnecessary code

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,20 +1,4 @@
-import { GeistSans } from "geist/font/sans";
-import { ThemeProvider } from "next-themes";
-import { DM_Sans } from "next/font/google";
-// import "./globals.css";
 import Navbar from "@/components/Navbar";
-
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
-
-export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "BunkMate - On Demand Subletting and Off-Campus Leasing",
-  description: "Find Off-campus housing and subletters with ease using Bunkmate.",
-};
-
-const dmsans = DM_Sans({ subsets: ["latin"] });
 
 export default async function RootLayout({
   children,
