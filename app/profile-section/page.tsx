39977efef5be0fa186110241aad25b3cc9033e2c
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2024-11-23 16:52:56 -0600

    Fix up the design implementation on profile page

--- app/profile-section/page.tsx
+++ app/profile-section/page.tsx
@@ -214,114 +214,113 @@ export default function Index() {
         <div className="flex flex-col items-center">
           <Navbar />
 
-          <main className="flex flex-col gap-[20px] w-full h-full items-left mb-20">
-            <div className="flex flex-col text-left sm:items-start ml-5">
-              <h1 className="text-left text-2xl font-semibold">Profile</h1>
+          <main className="flex flex-col sm:gap-[20px] w-full h-full items-left mb-20 ml-5">
+            <div className="flex flex-col text-left sm:items-start gap-4">
+              <h1 className="text-left text-3xl font-semibold">Profile</h1>
               <h1 className="text-left text-sm mb-2">
                 Welcome to your profile page! Here, you can access your profile
-                information, your favorites, and your lisitings
+                information, your favorites, and your listings.
               </h1>
             </div>
-            <div className="p-5 flex flex-col sm:flex-row gap-[42.7px]">
-              {/* Profile Image and Rice Affiliate text */}
-              <div className="flex flex-col items-center sm:items-start">
-                <h1 className="text-left text-2xl font-semibold mb-6">
-                  Your Profile Information
-                </h1>
-
-                <h1 className="text-lg sm:text-xl">Profile Picture</h1>
-                <div className="relative w-32 h-32 overflow-hidden rounded-full mb-5">
-                  <Image
-                    src={"/profile_pic.jpeg"}
-                    fill={true}
-                    alt="profile pic"
-                    className="object-cover"
-                  />
+            <div>
+              <h1 className="text-left text-2xl font-medium mb-6">
+                Your Profile Information
+              </h1>
+              <div className="p-5 flex flex-col sm:flex-row sm:gap-[24vh]">
+                {/* Profile Image and Rice Affiliate text */}
+                <div className="flex flex-col items-center sm:items-start gap-4 sm:gap-8">
+                  <div className="flex flex-col gap-4">
+                    <h1 className="text-lg sm:text-xl font-medium text-center sm:text-left">
+                      Profile Picture
+                    </h1>
+                    <div className="relative w-[24vh] h-[24vh] overflow-hidden rounded-full">
+                      <Image
+                        src={"/profile_pic.jpeg"}
+                        fill={true}
+                        alt="profile pic"
+                        className="object-cover"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col">
+                    <h1 className="text-lg font-medium">Rice Affiliation</h1>
+                    <div className="flex flex-row gap-[5px] items-center mt-2 sm:mt-0">
+                      <Image
+                        src={"/owl.png"}
+                        width={20}
+                        height={5}
+                        alt="owl"
+                        className="w-5 h-5 scale-75"
+                      />
+                      <p className="text-[#FF7439] text-sm">Rice Student</p>
+                    </div>
+                  </div>
                 </div>
-                <h1 className="text-lg font-semibold mt-10">Rice Affiliate</h1>
-                <div className="flex flex-row gap-[5.1px] items-center mt-2 sm:mt-0">
-                  <Image
-                    src={"/owl.png"}
-                    width={20}
-                    height={5}
-                    alt="owl"
-                    className="w-5 h-5 scale-75"
-                  />
-                  <p className="text-[#FF7439] text-sm">Rice Student</p>
+
+                {/* Additional Information */}
+                <div className="flex flex-col">
+                  <div className="flex flex-col gap-4">
+                    <div className="gap-4">
+                      <h1 className="text-lg font-medium">Name</h1>
+                      <p className="text-lg">{profile?.username}</p>
+                    </div>
+
+                    <div className="gap-4">
+                      <h1 className="text-lg font-medium">Email Address</h1>
+                      <p className="text-lg">{profile?.email}</p>
+                    </div>
+
+                    <div className="gap-4">
+                      <h1 className="text-lg font-medium">Phone Number</h1>
+                      <p className="text-lg">{profile?.phone}</p>
+                    </div>
+                  </div>
                 </div>
               </div>
+            </div>
 
-              {/* Additional Information */}
-              <div className="flex flex-col justify-center">
-                <div className="flex flex-col mt-4">
-                  <div className="flex flex-col mb-7">
-                    <h1 className="text-lg font-semibold mb-[2.27px]">Name</h1>
-                    <p className="text-lg">{profile?.username}</p>
+            <div>
+              <h1 className="text-left text-2xl font-medium">
+                Favorite Listings
+              </h1>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mb-6">
+                {favoritelistings.map((listing) => (
+                  <div key={listing.id} className="transform scale-90">
+                    <ListingCard
+                      postId={listing.id}
+                      name={listing.title}
+                      imagePath={listing.imageUrl}
+                      distance={listing.distance}
+                      duration={listing.dates}
+                      price={`$${listing.price} / month`}
+                      isRiceStudent={listing.renterType === "Rice Student"}
+                      isFavorited={listing.isFavorite}
+                    />
                   </div>
+                ))}
+              </div>
+            </div>
 
-                  <div className="mb-7">
-                    <h1 className="text-lg font-semibold mb-[2.27px]">
-                      Email Address
-                    </h1>
-                    <p className="text-xs text-gray-400">{profile?.email}</p>
+            <div>
+              <h1 className="text-left text-2xl font-medium">Your Listings</h1>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {listings.map((listing) => (
+                  <div key={listing.id} className="transform scale-90 -gap-1">
+                    <ListingCard
+                      postId={listing.id}
+                      name={listing.title}
+                      imagePath={listing.imageUrl}
+                      distance={listing.distance}
+                      duration={listing.dates}
+                      price={`$${listing.price} / month`}
+                      isRiceStudent={listing.renterType === "Rice Student"}
+                      isFavorited={listing.isFavorite}
+                    />
                   </div>
-
-                  <div className="mb-7 mt-8">
-                    <h1 className="text-lg font-semibold mb-[2.27px]">
-                      Phone Number
-                    </h1>
-                    <p className="text-xs text-gray-400">{profile?.phone}</p>
-                  </div>
-                </div>
+                ))}
               </div>
             </div>
           </main>
-
-          <div className="container">
-            <h1 className="text-left text-xl font-semibold mb-1">
-              Favorite Listings
-            </h1>
-          </div>
-
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mb-6">
-            {favoritelistings.map((listing) => (
-              <div key={listing.id} className="transform scale-90">
-                <ListingCard
-                  postId={listing.id}
-                  name={listing.title}
-                  imagePath={listing.imageUrl}
-                  distance={listing.distance}
-                  duration={listing.dates}
-                  price={`$${listing.price} / month`}
-                  isRiceStudent={listing.renterType === "Rice Student"}
-                  isFavorited={listing.isFavorite}
-                />
-              </div>
-            ))}
-          </div>
-
-          <div className="container">
-            <h1 className="text-left text-xl font-semibold mb-1 mt-10">
-              Your Listings
-            </h1>
-          </div>
-
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {listings.map((listing) => (
-              <div key={listing.id} className="transform scale-90 -gap-1">
-                <ListingCard
-                  postId={listing.id}
-                  name={listing.title}
-                  imagePath={listing.imageUrl}
-                  distance={listing.distance}
-                  duration={listing.dates}
-                  price={`$${listing.price} / month`}
-                  isRiceStudent={listing.renterType === "Rice Student"}
-                  isFavorited={listing.isFavorite}
-                />
-              </div>
-            ))}
-          </div>
         </div>
       </main>
     </>
