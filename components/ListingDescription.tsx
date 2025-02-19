44c0af4de15b3a7014fd6e114a2d18229d0c5aa1
1ucyhan <lucehanblank@gmail.com>
2025-02-18 23:49:47 -0600

    super simple name change

--- components/ListingDescription.tsx
+++ components/ListingDescription.tsx
@@ -82,7 +82,7 @@ const ListingDescription: React.FC<ListingDescriptionProps> = ({ data }) => {
         <div className="space-y-6">
           <DescriptionItem
             icon={<img src="/bed.svg" alt="Duration Icon" className="w-[40px] h-[40px]" />}
-            title="# of Beds and Bathrooms"
+            title="# of Beds and Baths"
             description={`${data.bed_num} beds, ${data.bath_num} bathrooms`}
             delay={0.5}
           />
