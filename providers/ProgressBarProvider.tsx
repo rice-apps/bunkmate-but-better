be595fd1d9aff9ec686748def6250b0890d38aba
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2025-02-21 17:24:53 -0600

    Add progress provider

--- providers/ProgressBarProvider.tsx
+++ providers/ProgressBarProvider.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
+import { AppProgressProvider as ProgressProvider } from "@bprogress/next";
 import { PropsWithChildren } from "react";
 
 const Providers = ({ children }: PropsWithChildren) => {
   return (
     <>
-    <ProgressBar
-      height="4px"
-      color="#FF7439"
-      options={{ showSpinner: true }}
-      shallowRouting
-    />
+      <ProgressProvider
+        height="10px"
+        color="#FF7439"
+        options={{ showSpinner: true }}
+        shallowRouting
+      />
       {children}
     </>
   );
