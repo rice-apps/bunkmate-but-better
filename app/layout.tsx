2751484a0a29c121ad83b93fba4520433450b0e4
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2025-02-24 01:58:56 -0600

    Add google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "next-themes";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import PostListingFormProvider from "@/providers/PostListingFormProvider";
+import { GoogleTagManager } from '@next/third-parties/google'
 import { Suspense } from "react";
 import ProgressBarProvider from "@/providers/ProgressBarProvider";
 
@@ -27,6 +28,7 @@ export default async function RootLayout({
   return (
     <Suspense>
       <html lang="en" className={GeistSans.className} suppressHydrationWarning>
+        <GoogleTagManager gtmId="G-F6DT1CZ9YT" />
         <body className={`${dmsans.className} bg-background text-foreground`}>
           <ThemeProvider
             attribute="class"
