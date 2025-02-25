55d5460f0cf4befb141601c763bceeba9809f162
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2025-02-25 14:26:19 -0600

    Inline hostname

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: new URL(process.env.NEXT_PUBLIC_SUPABASE_URL).hostname,
+        hostname: "*.supabase.co",
         pathname: "**",
       },
       {
