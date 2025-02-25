ff3c2f38fdba8a47d37a633640930019cd9e72dd
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2025-02-24 19:46:00 -0600

    Add supabase loader

---
+++ supabase-image-loader.js
@@ -0,0 +1,5 @@
+const projectId = 'lxuhpiqretvrbzrykivo'
+
+export default function supabaseLoader({ src, width, quality }) {
+  return `https://${projectId}.supabase.co/storage/v1/render/image/public/${src}?width=${width}&quality=${quality || 75}`
+}
