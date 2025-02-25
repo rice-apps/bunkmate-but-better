c030d2fe87b5cf3c2c230b5a71413e71da4da681
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2025-02-25 14:29:12 -0600

    retrigger again

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "*.supabase.co",
+        hostname: "*.supabase.co", 
         pathname: "**",
       },
       {
