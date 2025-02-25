046871dd695aac8826d0c8e412050af68bdadcfd
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2025-02-24 19:35:34 -0600

    Remove unneeded packages

--- next.config.js
+++ next.config.js
@@ -19,6 +19,18 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true, // to:do REMOVE THIS SHIT AS SOON AS POSSIBLE @SATHYA @GABE YES - LUCY
   },
+  compiler: {
+    removeConsole: {
+      exclude: ['error'],
+    },
+  },
+  experimental: {
+    optimizePackageImports: ['framer-motion', 'react-day-picker', '@supabase/ssr', '@supabase/supabase-js'],
+  }
 };
 
-module.exports = nextConfig;
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer(nextConfig);
